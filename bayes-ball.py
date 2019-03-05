a3c3676dd6f0d057dc193df72f769b0687500141
Pawel <pawelkostek1@gmail.com>
2019-03-06 03:18:52 +0800

    Begining to implement bayes-ball algorithm

--- bayes-ball.py
+++ bayes-ball.py
@@ -1,7 +1,7 @@
 '''
 Description: CS5340 - The Bayes-Ball algorithm
-Name: Your Name, Your partner's name
-Matric No.: Your matric number, Your partner's matric number
+Name: Pawel Kostkowski, Nirav Gandhi
+Matric No.: A0196329R, A********
 '''
 
 
@@ -40,9 +40,75 @@ def read_queries():
             queries.append([X, Y, Z])
     return queries
 
+def get_parents(graph, node):
+    """Helper function that return a list
+    of parents for a given node in a graph
+
+    Args:
+        graph (dict): the Bayesian network
+        node (int): a node of the graph
+
+    Returns:
+        parents(list): the list of parents for a given node
+    """
+    parents = []
+    return parents
+
+def mark_v_struct(graph, Z):
+    """Helper function that traverse the graph
+    from leaves to the roots, marking all nodes
+    that are in Z or have descendants in Z.
+
+    Args:
+        graph (dict): the Bayesian network
+        Z (list): list of nodes in set Z
+
+    Returns:
+        A (list): list of nodes that are in Z of have descendants in Z
+    """
+    L = Z
+    A = []
+    while len(L) != 0:
+        C = L.pop()
+        if C not in A:
+            for parent in get_parents(graph, C):
+                L = L.push(parent)
+        A = A.push(C)
+    return A
+
+def traverse_trails(graph, X, Y, Z):
+    """Helper function that traverse all the trails
+    in the graph looking for a blocked node.
+    Implemented as BFS algorithm
+
+    Args:
+        graph (dict): the Bayesian network
+        X (list): list of nodes in set X
+        Y (list): list of nodes in set Y
+        Z (list): list of nodes in set Z
+
+    Returns:
+
+    """
+    q = queue.Queue()
+    visited = {}
+
+    for x in X:
+        q.put(x) #this might be optimized as there might be node that will be visited multiple times
+    while not q.empty():
+        node = q.get()
+        visited[node] = True
+        if node in Y:
+            return False
+        if node not in Z:
+            for descendant in graph[node]:
+                if not visited[descendant]:
+                    q.put(descendant)
+    else:
+        return True
 
 def is_independent(graph, X, Y, Z):
-    """Checks if X is conditionally indepedent
+    """Checks if X is conditionally independent
     of Y given Z.
 
     Args:
@@ -52,11 +118,21 @@ def is_independent(graph, X, Y, Z):
         Z (list): list of nodes in set Z
 
     Returns:
-        bool: True if X is conditionally indepedent
+        bool: True if X is conditionally independent
     of Y given Z, False otherwise.
     """
-    # TODO
-    return True
+    #Phase 1
+    out = mark_v_struct(graph, Z)
+    for x in X:
+        if x in out:
+            return False
+    for y in Y:
+        if y in out:
+            return False
+    #Phase 2
+    ans = traverse_trails(graph, X, Y, out)
+
+    return ans
 
 
 if __name__ == '__main__':
