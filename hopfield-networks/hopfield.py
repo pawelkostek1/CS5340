d118efa488ef5bf3dfae06b5a94847396c59096f
Pawel <pawelkostek1@gmail.com>
2019-04-11 18:33:15 +0800

    Restructured repo to include the second coursework.

---
+++ hopfield-networks/hopfield.py
@@ -0,0 +1,141 @@
+'''
+Description: CS5340 - Hopfield Network
+Name: Your Name, Your partner's name
+Matric No.: Your matric number, Your partner's matric number
+'''
+
+
+import matplotlib
+matplotlib.use('Agg')
+import numpy as np
+import glob
+import matplotlib.pyplot as plt
+from PIL import Image, ImageOps
+
+
+
+def load_image(fname):
+    img = Image.open(fname).resize((32, 32))
+    img_gray = img.convert('L')
+    img_eq = ImageOps.autocontrast(img_gray)
+    img_eq = np.array(img_eq.getdata()).reshape((img_eq.size[1], -1))
+    return img_eq
+
+
+def binarize_image(img_eq):
+    img_bin = np.copy(img_eq)
+    img_bin[img_bin < 128] = -1
+    img_bin[img_bin >= 128] = 1
+    return img_bin
+
+
+def add_corruption(img):
+    img = img.reshape((32, 32))
+    t = np.random.choice(3)
+    if t == 0:
+        i = np.random.randint(32)
+        img[i:(i + 8)] = -1
+    elif t == 1:
+        i = np.random.randint(32)
+        img[:, i:(i + 8)] = -1
+    else:
+        mask = np.sum([np.diag(-np.ones(32 - np.abs(i)), i)
+                       for i in np.arange(-4, 5)], 0).astype(np.int)
+        img[mask == -1] = -1
+    return img.ravel()
+
+
+def learn_hebbian(imgs):
+    img_size = np.prod(imgs[0].shape)
+    ######################################################################
+    ######################################################################
+    weights = np.zeros((img_size, img_size))
+    bias = np.zeros(img_size)
+    # Complete this function
+    # You are allowed to modify anything between these lines
+    # Helper functions are allowed
+    #######################################################################
+    #######################################################################
+    return weights, bias
+
+
+def learn_maxpl(imgs):
+    img_size = np.prod(imgs[0].shape)
+    ######################################################################
+    ######################################################################
+    weights = np.zeros((img_size, img_size))
+    bias = np.zeros(img_size)
+    # Complete this function
+    # You are allowed to modify anything between these lines
+    # Helper functions are allowed
+    #######################################################################
+    #######################################################################
+    return weights, bias
+
+
+def plot_results(imgs, cimgs, rimgs, fname='result.png'):
+    '''
+    This helper function can be used to visualize results.
+    '''
+    img_dim = 32
+    assert imgs.shape[0] == cimgs.shape[0] == rimgs.shape[0]
+    n_imgs = imgs.shape[0]
+    fig, axn = plt.subplots(n_imgs, 3, figsize=[8, 8])
+    for j in range(n_imgs):
+        axn[j][0].axis('off')
+        axn[j][0].imshow(imgs[j].reshape(img_dim, img_dim), cmap='Greys_r')
+    axn[0, 0].set_title('True')
+    for j in range(n_imgs):
+        axn[j][1].axis('off')
+        axn[j][1].imshow(cimgs[j].reshape(img_dim, img_dim), cmap='Greys_r')
+    axn[0, 1].set_title('Corrupted')
+    for j in range(n_imgs):
+        axn[j][2].axis('off')
+        axn[j][2].imshow(rimgs[j].reshape((img_dim, img_dim)), cmap='Greys_r')
+    axn[0, 2].set_title('Recovered')
+    fig.tight_layout()
+    plt.savefig(fname)
+
+
+def recover(cimgs, W, b):
+    img_size = np.prod(cimgs[0].shape)
+    ######################################################################
+    ######################################################################
+    rimgs = []
+    # Complete this function
+    # You are allowed to modify anything between these lines
+    # Helper functions are allowed
+    #######################################################################
+    #######################################################################
+    return rimgs
+
+
+def main():
+    # Load Images and Binarize
+    ifiles = sorted(glob.glob('images/*'))
+    timgs = [load_image(ifile) for ifile in ifiles]
+    imgs = np.asarray([binarize_image(img) for img in timgs])
+    for img in (imgs):
+        plt.figure(0)
+        plt.imshow(img)
+
+
+    # Add corruption
+    cimgs = []
+    for i, img in enumerate(imgs):
+        cimgs.append(add_corruption(np.copy(imgs[i])))
+    cimgs = np.asarray(cimgs)
+
+    # Recover 1 -- Hebbian
+    Wh, bh = learn_hebbian(imgs)
+    rimgs_h = recover(cimgs, Wh, bh)
+    np.save('hebbian.npy', rimgs_h)
+
+    # Recover 2 -- Max Pseudo Likelihood
+    Wmpl, bmpl = learn_maxpl(imgs)
+    rimgs_mpl = recover(cimgs, Wmpl, bmpl)
+    np.save('mpl.npy', rimgs_mpl)
+
+
+if __name__ == '__main__':
+    main()
